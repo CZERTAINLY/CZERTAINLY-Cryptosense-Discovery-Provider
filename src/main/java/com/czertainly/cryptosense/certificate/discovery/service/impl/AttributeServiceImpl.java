78a1d563f5d943d1f867472febae0646c0efa133
3KeyPradeep <pradeep.saminathan@3key.company>
2023-01-04 23:35:31 +0530

    Update Constraint creation without type explicitly
    Update Constraint creation without type explicitly

--- src/main/java/com/czertainly/cryptosense/certificate/discovery/service/impl/AttributeServiceImpl.java
+++ src/main/java/com/czertainly/cryptosense/certificate/discovery/service/impl/AttributeServiceImpl.java
@@ -93,7 +93,6 @@ public class AttributeServiceImpl implements AttributeService {
         apiUrl.setConstraints(List.of(new RegexpAttributeConstraint(
                 "Cryptosense Analyzer URL",
                 "Enter the valid URL",
-                AttributeConstraintType.REGEXP,
                 "^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$")));
         return apiUrl;
     }
